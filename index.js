72f2192ad578017ff453730bd411dccdb09ac06f
nithinvarma <nithinvarma411@gmail.com>
2025-01-29 15:42:23 +0530

    completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = 3010;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB Atlas');
+  } catch (error) {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  }
+};
+
+connectDB()
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
 
 app.use(express.static('static'));
 
